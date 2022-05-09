9b200d72337e195d94bf34bbeb9a085239d9940a
halilesmer <taximinator@gmail.com>
2022-05-09 09:12:22 +0200

    connect with mongodb atlas

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,20 @@
+//IMPORTS
 //const express = require("express");
 import express from "express";
-
 //const bodyParser = require('body-parser');
 import bodyParser from "body-parser";
-
 //const routesHandler = require('./routes/handler.js');
 import routesHandler from "./routes/handler.js";
-
+//const cors = require("cors");
+import cors from 'cors';
 const app = express();
+
+//MIDDLEWARE
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use("/", routesHandler);
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/", routesHandler); 
 const PORT = 5000;
 
 app.listen(PORT, () => {

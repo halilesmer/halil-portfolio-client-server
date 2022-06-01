8ac28db361b7afa009ff1265648c3d7ead340a2c
halilesmer <taximinator@gmail.com>
2022-06-01 19:08:57 +0200

    added zoom buttons to Resume

--- client/src/components/Resume.js
+++ client/src/components/Resume.js
@@ -1,14 +1,16 @@
 
 import React, { useState } from 'react'
 
-// Import Worker
-import { Worker } from '@react-pdf-viewer/core';
-// Import the main Viewer component
-import { Viewer } from '@react-pdf-viewer/core';
+// Import Worker and the main Viewer component
+import { Worker, Viewer } from '@react-pdf-viewer/core';
+import { zoomPlugin } from '@react-pdf-viewer/zoom';
+
+
 // Import the styles
 import '@react-pdf-viewer/core/lib/styles/index.css';
 // Import styles of default layout plugin
 import '@react-pdf-viewer/default-layout/lib/styles/index.css';
+import '@react-pdf-viewer/zoom/lib/styles/index.css';
 
 import pdf from '../assets/pdf/2022-05-30 CV_Halil_en.pdf'
 
@@ -35,15 +37,32 @@ export const Resume = () => {
   // }, []);
  
 
+  const zoomPluginInstance = zoomPlugin();
+  const { ZoomInButton, ZoomOutButton, ZoomPopover } = zoomPluginInstance;
+
   return (
     <div className="container">
       {/* View PDF */}
       <div className="viewer">
-
+      <div
+          style={{
+            alignItems: "center",
+            backgroundColor: "#eeeeee",
+            borderBottom: "1px solid rgba(0, 0, 0, 0.1)",
+            display: "flex",
+            justifyContent: "center",
+            padding: "4px"
+          }}
+        >
+          <ZoomOutButton />
+          <ZoomPopover />
+          <ZoomInButton />
+        </div>
         {/* render this if we have a pdf file */}
         {pdfFile && (
           <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.14.305/build/pdf.worker.min.js">
             <Viewer fileUrl={pdfFile}
+           plugins={[zoomPluginInstance]} 
               theme={{
                 theme: 'auto',
               }}

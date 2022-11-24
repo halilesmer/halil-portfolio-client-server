c7c1fcc247cac217855ce5722ff67fd962bba256
halilesmer <halil@esmer.de>
2022-11-24 10:15:38 +0100

    some refactorings

--- client/src/index.js
+++ client/src/index.js
@@ -1,27 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
-
-
 import "normalize.css";
 import "./index.css";
+
 import App from "./App.js";
-import reportWebVitals from "./reportWebVitals.js";
+import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
+import { createRoot } from "react-dom/client";
+import reportWebVitals from "./reportWebVitals.js";
 
-/* const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const root = document.querySelector("#root");
 
-     <Router>
+createRoot(root).render(
+  <React.StrictMode>
+    <Router>
       <App />
     </Router>
-
-); */
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <Router>
-      <App />
-    </Router>
+  </React.StrictMode>
 );
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
